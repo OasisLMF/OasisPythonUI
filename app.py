9b1ada108e478b591bda83c5008c481b7064a3b0
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2024-12-12 12:27:08 +0000

    Display portfolios

--- app.py
+++ app.py
@@ -1,4 +1,41 @@
 import streamlit as st
+from oasislmf.platform_api.client import APIClient
+import pandas as pd
 
+st.markdown("# OasisLMF UI")
 
-st.write("Hello world")
+@st.cache_resource
+def get_client():
+    return APIClient()
+
+client = get_client()
+
+st.markdown("## Portfolios")
+
+portfolios = client.portfolios.get().json()
+portfolios =  pd.json_normalize(portfolios)
+portfolios['created'] = pd.to_datetime(portfolios['created'])
+portfolios['modified'] = pd.to_datetime(portfolios['modified'])
+
+@st.cache_data
+def generate_column_config(col_names):
+    columns = [
+        'id',
+        'name',
+        'created',
+        'modified',
+        'storage_links',
+    ]
+
+    config = {name: None for name in col_names}
+    for c in columns:
+        config[c] = c.replace('_', ' ').title()
+    return config
+
+column_config = generate_column_config(portfolios.columns.values)
+column_config['created'] = st.column_config.DatetimeColumn(column_config['created'],
+                                                           format="DD/MM/YY HH:mm:ss")
+column_config['modified'] = st.column_config.DatetimeColumn(column_config['modified'],
+                                                           format="DD/MM/YY HH:mm:ss")
+
+st.dataframe(portfolios, hide_index=True, column_config=column_config)
