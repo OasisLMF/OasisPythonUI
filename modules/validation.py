4197e9a1fd3ed9803ad5d525c5e54417e4291ba8
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2024-12-16 12:57:30 +0000

    Add dict like validation

--- modules/validation.py
+++ modules/validation.py
@@ -7,3 +7,13 @@ def validate_name(name):
     if not name or len(name) == 0:
         return False, "Name is required"
     return True, ""
+
+def validate_col_val(df, col_name, val):
+    if df[col_name] == val:
+        return True, ""
+    return False, "Column {col_name} has incorrect value."
+
+def validate_key_is_not_null(df, col):
+    if df[col] is None:
+        return False, f"{col} is None"
+    return True, ""
