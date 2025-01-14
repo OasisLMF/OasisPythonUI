a2ad7635d7e212c85629544d74ecddd9b620fa6b
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-01-14 16:32:56 +0000

    add create analyses form popover to idf

--- pages/idf.py
+++ pages/idf.py
@@ -2,6 +2,8 @@ import streamlit as st
 from modules.nav import SidebarNav
 from modules.client import get_portfolios, get_analyses, get_models
 from pages.components.display import DataframeView
+from pages.components.create import create_analysis_form
+from modules.validation import process_validations, validate_not_none
 
 st.set_page_config(
     page_title = "IDF Demo",
@@ -23,9 +25,9 @@ datetime_cols = ['created', 'modified']
 display_cols = [ 'id', 'name', 'created', 'modified', ]
 
 portfolios = get_portfolios(client)
-portfolio_view = DataframeView(portfolios, display_cols=display_cols)
+portfolio_view = DataframeView(portfolios, display_cols=display_cols, selectable=True)
 portfolio_view.convert_datetime_cols(datetime_cols)
-portfolio_view.display()
+selected_portfolio = portfolio_view.display()
 
 '## Models'
 models = get_models(client)
@@ -34,7 +36,15 @@ datetime_cols = ['created', 'modified']
 
 model_view = DataframeView(models, selectable=True, display_cols=display_cols)
 model_view.convert_datetime_cols(datetime_cols)
-selected = model_view.display()
+selected_model = model_view.display()
+
+validations_list = [(validate_not_none, [selected_portfolio, 'Portfolio']),
+                    (validate_not_none, [selected_model, 'Model'])]
+enable_popover, msg = process_validations(validations_list)
+
+with st.popover("Create Analysis", disabled=not enable_popover, help=msg):
+    create_analysis_form(portfolios=[selected_portfolio], models=[selected_model], client=client)
+
 
 '## Analyses'
 analyses = get_analyses(client)
