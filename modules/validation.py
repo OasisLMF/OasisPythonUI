bf5071788914ad336653baacc065defdc8d7b0f7
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-01-14 16:32:02 +0000

    typo in validations

--- modules/validation.py
+++ modules/validation.py
@@ -1,11 +1,11 @@
 def validate_not_none(param, paramName='parameter'):
     if param is None:
-        return False, f'{paramName} is not set'
+        return False, f'{paramName} is not set.'
     return True, ""
 
 def validate_name(name, paramName="Name"):
     if not name or len(name) == 0:
-        return False, f"{paramName} is required"
+        return False, f"{paramName} is required."
     return True, ""
 
 def validate_key_vals(df, key, vals):
@@ -15,7 +15,7 @@ def validate_key_vals(df, key, vals):
 
 def validate_key_is_not_null(df, col):
     if df[col] is None:
-        return False, f"{col} is None"
+        return False, f"{col} is None."
     return True, ""
 
 def process_validations(validations):
