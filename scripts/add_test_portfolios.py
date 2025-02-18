4f1e5bb21c1a634fbe79c0a9634f6d8e84b89db6
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-02-18 11:39:07 +0000

    extensible add portfolio script

--- scripts/add_test_portfolios.py
+++ scripts/add_test_portfolios.py
@@ -1,32 +1,33 @@
 from oasislmf.platform_api.client import APIClient
 import os
 import logging
+import sys
+import streamlit as st
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-def main():
-    # Initialise client
-    logger.info("Initialising client")
-    api_url = os.environ.get('API_URL', 'http://localhost:8000')
-    username = "admin"
-    password = "password"
-    client = APIClient(username=username, password=password, api_url=api_url)
+def add_portfolio(client, input_args):
+    existing_names = [r['name'] for r in client.portfolios.get().json()]
+    logger.info('Adding portfolios...')
+
+    if input_args.get('name') and input_args.get('name') in existing_names:
+        logger.info(f'Skipping {input_args["name"]}')
+    else:
+        logger.info(f'Adding {input_args["portfolio_name"]}')
+        client.upload_inputs(**input_args)
 
 
-    logger.info("Finding PiWind...")
-    # Get portfolio files
-    piwind_path = './OasisPiWind/'
-
+def add_piwind_small(client):
+    path = './OasisPiWind/'
     # Check if PiWind can be found
-    if not os.path.exists(piwind_path):
-
+    if not os.path.exists(path):
         logger.error("PiWind not found")
         return 1
 
-    input_path = os.path.join(piwind_path, 'tests/inputs')
+    input_path = os.path.join(path, 'tests/inputs')
 
-    piwind_small_args = {
+    input_args = {
         'portfolio_name': 'piwind-small',
         'location_f': os.path.join(input_path, 'SourceLocOEDPiWind10.csv'),
         'accounts_f': os.path.join(input_path, 'SourceAccOEDPiWind.csv'),
@@ -34,22 +35,44 @@ def main():
         'ri_scope_f': os.path.join(input_path, 'SourceReinsScopeOEDPiWind.csv'),
     }
 
-    piwind_large_args = piwind_small_args.copy()
-    piwind_large_args['portfolio_name'] = 'piwind-large'
-    piwind_large_args['location_f'] = os.path.join(input_path, 'SourceLocOEDPiWind.csv')
+    add_portfolio(client, input_args)
 
-    existing_names = [r['name'] for r in client.portfolios.get().json()]
-    logger.info('Adding portfolios...')
-    if piwind_small_args['portfolio_name'] in existing_names:
-        logger.info(f'Skipping {piwind_small_args["portfolio_name"]}')
-    else:
-        logger.info(f'Adding {piwind_small_args["portfolio_name"]}')
-        client.upload_inputs(**piwind_small_args)
-    if piwind_large_args['portfolio_name'] in existing_names:
-        logger.info(f'Skipping {piwind_large_args["portfolio_name"]}')
-    else:
-        logger.info(f'Adding {piwind_large_args["portfolio_name"]}')
-        client.upload_inputs(**piwind_large_args)
+
+def add_piwind_large(client):
+    path = './OasisPiWind/'
+    # Check if PiWind can be found
+    if not os.path.exists(path):
+        logger.error("PiWind not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/inputs')
+
+    input_args = {
+        'portfolio_name': 'piwind-large',
+        'location_f': os.path.join(input_path, 'SourceLocOEDPiWind.csv'),
+        'accounts_f': os.path.join(input_path, 'SourceAccOEDPiWind.csv'),
+        'ri_info_f': os.path.join(input_path, 'SourceReinsInfoOEDPiWind.csv'),
+        'ri_scope_f': os.path.join(input_path, 'SourceReinsScopeOEDPiWind.csv'),
+    }
+
+    add_portfolio(client, input_args)
 
 if __name__=="__main__":
-    main()
+    logger.info("Initialising client")
+    api_url = os.environ.get('API_URL', 'http://localhost:8000')
+    user = st.secrets.get('user', 'admin')
+    password = st.secrets.get('password', 'password')
+    client =  APIClient(api_url=api_url, username=user, password=password)
+    if len(sys.argv) > 1:
+        portfolios = sys.argv[1:]
+    else:
+        portfolios = ['piwind-small', 'piwind-large']
+
+    function_map = {
+        'piwind-small':  add_piwind_small,
+        'piwind-large': add_piwind_large
+    }
+
+    for p in portfolios:
+        assert p in function_map.keys(), 'Portfolio not found'
+        function_map[p](client)
