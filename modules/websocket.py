b01cc0ea3a380d8a0e7b8f9a08288a31ea932831
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-04-14 17:30:22 +0100

    continously read websocket

--- modules/websocket.py
+++ modules/websocket.py
@@ -1,12 +1,19 @@
 import asyncio
-from websockets.sync.client import connect
 from oasislmf.platform_api.client import APIClient
+import websockets
 
+async def listen_to_websocket(uri, headers):
+    async with websockets.connect(uri, additional_headers=headers, ping_interval=None) as websocket:
+        print(f"Connected to {uri}")
+        try:
+            while True:
+                message = await websocket.recv()
+                print(f"Received: {message}")
+        except websockets.exceptions.ConnectionClosed:
+            print("WebSocket connection closed")
 
-def hello():
-    ws_path = 'ws://localhost:8001/ws/v2/queue-status/'
+if __name__ == "__main__":
+    uri = 'ws://localhost:8001/ws/v2/queue-status/'
     oasis_client = APIClient()
     headers = {'AUTHORIZATION': f'Bearer {oasis_client.api.tkn_access}'}
-    with connect(ws_path, additional_headers=headers, ping_interval=None) as ws:
-        message = ws.recv()
-        print(message)
+    asyncio.run(listen_to_websocket(uri, headers))
