b33ae057092b344415e42c61bf245fba3bbd799c
Vinul <vinul.wimalaweera@oasislmf.org>
2025-05-09 11:07:03 +0000

    sync scenarios instance + github

--- scripts/add_test_portfolios.py
+++ scripts/add_test_portfolios.py
@@ -37,6 +37,25 @@ def add_piwind_small(client):
 
     add_portfolio(client, input_args)
 
+def add_piwind_small_alt(client):
+    path = './OasisPiWind/'
+    # Check if PiWind can be found
+    if not os.path.exists(path):
+        logger.error("PiWind not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/inputs')
+
+    input_args = {
+        'portfolio_name': 'piwind-small-alt',
+        'location_f': os.path.join(input_path, 'SourceLocOEDPiWind10Alt.csv'),
+        'accounts_f': os.path.join(input_path, 'SourceAccOEDPiWind.csv'),
+        'ri_info_f': os.path.join(input_path, 'SourceReinsInfoOEDPiWind.csv'),
+        'ri_scope_f': os.path.join(input_path, 'SourceReinsScopeOEDPiWind.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
 
 def add_piwind_large(client):
     path = './OasisPiWind/'
@@ -57,6 +76,128 @@ def add_piwind_large(client):
 
     add_portfolio(client, input_args)
 
+def add_maeq(client):
+    pname = 'maeq-test'
+    path = '../models/MAEQ/1.0.0/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/')
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(input_path, 'MAEQ_100_oasis.csv'),
+        'accounts_f': os.path.join(input_path, 'MAEQ_acc.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
+def add_trek(client):
+    pname = 'treq-test'
+    path = '../models/TREQ/1.0.0/'
+
+    if not os.path.exists(path):
+        print(path)
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/')
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(input_path, 'TREQ_P_test_loc_100.csv'),
+        'accounts_f': os.path.join(input_path, 'TREQ_acc.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
+def add_maeq_lat_long(client):
+    pname = 'maeq-test-ll'
+    path = '../models/MAEQ/1.0.0/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/')
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(input_path, 'MAEQ_100_oasis_lat_long.csv'),
+        'accounts_f': os.path.join(input_path, 'MAEQ_acc.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
+def add_euws_lat_long(client):
+    pname = 'euws-test-ll'
+    path = '../models/EUWS/1.0.0/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/')
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(input_path, 'EUWS_200_oasis_lat_long.csv'),
+        'accounts_f': os.path.join(input_path, 'EUWS_acc.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
+def add_euws(client):
+    pname = 'euws-test'
+    path = '../models/EUWS/1.0.0/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_path = os.path.join(path, 'tests/')
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(input_path, 'EUWS_200_oasis.csv'),
+        'accounts_f': os.path.join(input_path, 'EUWS_acc.csv'),
+    }
+
+    add_portfolio(client, input_args)
+
+def add_phty_taiwan(client):
+    pname = 'phty-taiwan'
+    path = '../models/PHTY/tests/inputs/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(path, 'locationTW_diff3.csv')
+    }
+
+    add_portfolio(client, input_args)
+
+def add_us_flat(client):
+    pname = 'us-flat'
+    path = '../models/US_Hurricane/tests/test_1/'
+
+    if not os.path.exists(path):
+        logger.error(f"{pname} not found")
+        return 1
+
+    input_args = {
+        'portfolio_name': pname,
+        'location_f': os.path.join(path, 'locationFlat.csv')
+    }
+
+    add_portfolio(client, input_args)
+
+
 if __name__=="__main__":
     logger.info("Initialising client")
     api_url = os.environ.get('API_URL', 'http://localhost:8000')
@@ -70,7 +211,15 @@ if __name__=="__main__":
 
     function_map = {
         'piwind-small':  add_piwind_small,
-        'piwind-large': add_piwind_large
+        'piwind-large': add_piwind_large,
+        'piwind-small-alt': add_piwind_small_alt,
+        'maeq': add_maeq,
+        'maeq-ll': add_maeq_lat_long,
+        'euws': add_euws,
+        'euws-ll': add_euws_lat_long,
+        'trek' : add_trek,
+        'phty-tw': add_phty_taiwan,
+        'us-flat': add_us_flat
     }
 
     for p in portfolios:
