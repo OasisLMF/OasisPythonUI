72bb8112b5a71104ef2cc55f4ff2e0b2da7bb2be
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-01 15:10:47 +0100

    add deploy all and full reset options

--- scenarios/deploy_scenarios_ui.sh
+++ scenarios/deploy_scenarios_ui.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 echo "DEPLOY_UI: $DEPLOY_UI"
+echo "DEPLOY_ALL: $DEPLOY_ALL"
 echo "UI_PATH: $UI_PATH"
+echo "WIPE: $WIPE"
 
-if [ "$DEPLOY_UI" = 'true' ]; then
+if [[ "$WIPE" = 'true' ]] && [[ "$DEPLOY_ALL" = 'false' ]]; then
+  echo "WIPE without DEPLOY_ALL not allowed."
+  exit 1
+fi
+
+if [[ "$WIPE" = 'true' ]]; then
+    printf "Deleting docker container:\n"
+    set +e
+    docker compose -f "$UI_PATH/oasis-scenarios-platform.yml" -f "$UI_PATH/oasis-scenarios-ui.yml" down --remove-orphans
+    set -e
+    printf "Deleting docker data: \n"
+    docker volume ls | grep OasisData | awk 'BEGIN { FS = "[ \t\n]+" }{ print $2 }' | xargs -r docker volume rm
+fi
+
+if [[ "$DEPLOY_UI" = 'true' ]] && [[ "$DEPLOY_ALL" != 'true' ]]; then
   docker image pull coreoasis/oasis_scenarios
   docker compose -f "$UI_PATH/oasis-scenarios-ui.yml" up -d
+fi
+
+if [[ "$DEPLOY_ALL" = 'true' ]]; then
+  docker pull coreoasis/model_worker:latest
+  docker pull coreoasis/api_server:latest
+  docker pull coreoasis/oasis_scenarios:latest
+  docker compose -f "$UI_PATH/oasis-scenarios-platform.yml" up -d --no-build
+  docker compose -f "$UI_PATH/oasis-scenarios-ui.yml" up -d --no-build
 fi
