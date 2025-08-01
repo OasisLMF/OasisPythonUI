bb5110919e34b1a2a1f52d46b5dc64cdcc131233
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-01 17:15:27 +0100

    reorganise deploy script

--- scenarios/deploy_scenarios_ui.sh
+++ scenarios/deploy_scenarios_ui.sh
@@ -10,21 +10,22 @@ if [[ "$WIPE" = 'true' ]] && [[ "$DEPLOY_ALL" = 'false' ]]; then
   exit 1
 fi
 
-if [[ "$WIPE" = 'true' ]]; then
-    printf "Deleting docker container:\n"
-    set +e
-    docker compose -f "$UI_PATH/oasis-scenarios-platform.yml" -f "$UI_PATH/oasis-scenarios-ui.yml" down --remove-orphans
-    set -e
-    printf "Deleting docker data: \n"
-    docker volume ls | grep OasisData | awk 'BEGIN { FS = "[ \t\n]+" }{ print $2 }' | xargs -r docker volume rm
-fi
-
 if [[ "$DEPLOY_UI" = 'true' ]] && [[ "$DEPLOY_ALL" != 'true' ]]; then
   docker image pull coreoasis/oasis_scenarios
   docker compose -f "$UI_PATH/oasis-scenarios-ui.yml" up -d
 fi
 
 if [[ "$DEPLOY_ALL" = 'true' ]]; then
+  printf "Deleting docker container:\n"
+  set +e
+  docker compose -f "$UI_PATH/oasis-scenarios-platform.yml" -f "$UI_PATH/oasis-scenarios-ui.yml" down --remove-orphans
+  set -e
+  if [[ "$WIPE" = 'true' ]]; then
+      printf "Deleting docker data: \n"
+      docker volume ls | grep OasisData | awk 'BEGIN { FS = "[ \t\n]+" }{ print $2 }' | xargs -r docker volume rm
+  fi
+  docker compose -f "$UI_PATH/oasis-scenarios-platform.yml" down
+  docker compose -f "$UI_PATH/oasis-scenarios-ui.yml" down
   docker pull coreoasis/model_worker:latest
   docker pull coreoasis/api_server:latest
   docker pull coreoasis/oasis_scenarios:latest
