048f5e21821729cb3397449a01506e24eb8787fd
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-01 16:25:48 +0100

    add retries to add portfolios

--- scripts/add_test_portfolios.py
+++ scripts/add_test_portfolios.py
@@ -1,10 +1,14 @@
+from oasis_data_manager.errors import OasisException
 from oasislmf.platform_api.client import APIClient
+from requests.exceptions import ConnectionError
 import os
 import logging
+from requests.exceptions import HTTPError
 import streamlit as st
 import argparse
 from pathlib import Path
 import json
+import time
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -19,20 +23,49 @@ def add_portfolio(client, input_args):
         logger.info(f'Adding {input_args["portfolio_name"]}')
         client.upload_inputs(**input_args)
 
-if __name__=="__main__":
-    logger.info("Initialising client")
-    api_url = os.environ.get('API_URL', 'http://localhost:8000')
-    user = st.secrets.get('user', 'admin')
-    password = st.secrets.get('password', 'password')
-    client =  APIClient(api_url=api_url, username=user, password=password)
-
+def main():
     parser = argparse.ArgumentParser(description='Script to add portfolios')
     parser.add_argument('-c', '--config', default='./portfolios.json',
                         help='Path to portfolios config file.', type=Path)
     parser.add_argument('-p', '--portfolios', nargs='+', default=None,
                         help='Portfolio(s) to add as described by config file. By default adds all portfolios')
+    parser.add_argument('-r', '--retry', action='store_true',
+                        help='Retry initialising client')
+    parser.add_argument('-m', '--max-retries', default=10, type=int,
+                        help='Max number of retries')
+    parser.add_argument('-i', '--interval-retries', default=10, type=int,
+                        help='Interval between retries in seconds.')
+
+    logger.info("Initialising client")
+    api_url = os.environ.get('API_URL', 'http://localhost:8000')
+    user = st.secrets.get('user', 'admin')
+    password = st.secrets.get('password', 'password')
 
     args = vars(parser.parse_args())
+
+    retry_count = 0
+    if args.get('retry'):
+        max_retries = args.get('max_retries')
+        logger.info(f'max retries: {max_retries}')
+    else:
+        logger.info('No retries')
+        max_retries = 1
+
+    while True:
+        try:
+            client =  APIClient(api_url=api_url, username=user, password=password)
+            break
+        except (ConnectionError, OasisException) as e:
+            logger.error(f'Retry: {retry_count+1}/{max_retries}.\nFailed to load client: {e}')
+
+        retry_count += 1
+
+        if max_retries <= retry_count:
+            print('Failed to load client.')
+            return
+
+        time.sleep(args.get('interval_retries'))
+
 
     with open(args['config'], 'r') as f:
         config = json.load(f)
@@ -49,3 +82,6 @@ if __name__=="__main__":
     for p in portfolios:
         input_args = config[p]
         add_portfolio(client, input_args)
+
+if __name__=="__main__":
+    main()
