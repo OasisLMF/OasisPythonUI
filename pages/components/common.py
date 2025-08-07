7b581abe09c02a5ae8793ae685cad4e009103083
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-07 11:47:58 +0100

    add pages/components/common.py

---
+++ pages/components/common.py
@@ -0,0 +1,9 @@
+"""
+File containig some common methods for `pages/`.
+"""
+
+PERSPECTIVES_MAP = {
+        'gul': 'Group-up loss',
+        'il': 'Insured loss',
+        'ri': 'Reinsured loss'
+        }
